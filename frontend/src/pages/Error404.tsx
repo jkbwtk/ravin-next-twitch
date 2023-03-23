9415e6eaf5eaeb90700f75399a1effdcd0721f35
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2023-03-23 08:47:32 +0100

    Fixed minor styling issues

--- frontend/src/pages/Error404.tsx
+++ frontend/src/pages/Error404.tsx
@@ -7,7 +7,7 @@ import Link from '#components/Link';
 const Error404: Component = () => {
   return (
     <div class={style.container}>
-      <span class={style.code}>404</span>
+      <span class={style.header}>404</span>
       <span class={style.message}>Page not found</span>
 
       <Link href='/' customClass={style.button} symbol='home' size='big' color='primary'>
