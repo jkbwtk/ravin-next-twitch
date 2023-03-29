94d9fd55c8c19da55f6481f611ccb959ca058736
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2023-03-29 03:07:35 +0200

    Added Anchor component

---
+++ frontend/src/components/Anchor.tsx
@@ -0,0 +1,34 @@
+import { JSX } from 'solid-js';
+import { mergeProps } from 'solid-js';
+import ButtonBase, {
+  ButtonBaseExcludedProps,
+  getColorClass,
+  getSizeClass,
+} from '#components/ButtonBase';
+import { ButtonProps, defaultProps } from '#components/Button';
+
+import style from '#styles/ButtonBase.module.scss';
+
+
+const Anchor: Component<ButtonProps & Omit<JSX.HTMLAttributes<HTMLAnchorElement>, ButtonBaseExcludedProps>> = (userProps) => {
+  const props = mergeProps(defaultProps, userProps);
+
+  const colorClass = getColorClass(props.color);
+  const sizeClass = getSizeClass(props.size);
+
+  return (
+    <a
+      classList={{
+        [style.button]: true,
+        [colorClass]: true,
+        [sizeClass]: true,
+        [props.customClass]: true,
+      }}
+      {...props}
+    >
+      <ButtonBase {...props} />
+    </a>
+  );
+};
+
+export default Anchor;
