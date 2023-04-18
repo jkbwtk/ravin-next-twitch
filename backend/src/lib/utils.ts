497c2f3638fc908d606699f1e4f88ba93b9b5684
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2023-04-19 01:35:31 +0200

    Added new util functions/types

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -36,3 +36,20 @@ export const clamp = (min: number, max: number, val: number): number => {
 };
 
 export const enumerate = <T>(array: Array<T>): [number, T][] => array.map((v, k) => [k, v]);
+
+// https://stackoverflow.com/questions/48230773/how-to-create-a-partial-like-that-requires-a-single-property-to-be-set/48244432
+export type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
+
+// https://stackoverflow.com/questions/40510611/typescript-interface-require-one-of-two-properties-to-exist
+export type AtLeastOneOf<T, Keys extends keyof T = keyof T> =
+  Pick<T, Exclude<keyof T, Keys>> &
+  { [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>> }[Keys];
+
+export const randomAlphanumeric = (length: number): string => {
+  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+
+  for (let i = 0; i < length; i += 1) result += chars.charAt(Math.floor(Math.random() * chars.length));
+
+  return result;
+};
