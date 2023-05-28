d2b86f3b55ba9fe7233bea6a764d35d4446f1383
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2023-05-28 14:23:00 +0200

    Removed unused imports

--- frontend/src/components/DashboardInfoBar.tsx
+++ frontend/src/components/DashboardInfoBar.tsx
@@ -1,4 +1,4 @@
-import { createMemo, createResource, For, Match, Show, Switch } from 'solid-js';
+import { createMemo, createResource, For, Show } from 'solid-js';
 import MaterialSymbol from '#components/MaterialSymbol';
 import { Link, useRouteData } from '@solidjs/router';
 import { SidebarRoute } from '#components/DashboardSidebar/SidebarElementBase';
