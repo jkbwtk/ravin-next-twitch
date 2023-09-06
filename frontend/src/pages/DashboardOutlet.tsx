8c11e406b3759f46f47f2d4ee951427c6c691ae7
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2023-09-06 19:06:33 +0200

    Improved routing responsiveness

--- frontend/src/pages/DashboardOutlet.tsx
+++ frontend/src/pages/DashboardOutlet.tsx
@@ -1,11 +1,20 @@
 import DashboardPage from '#components/DashboardPage';
-import { Outlet } from '@solidjs/router';
+import DotSpinner from '#components/DotSpinner';
+import FetchFallback from '#components/FetchFallback';
+import { Outlet, useIsRouting } from '@solidjs/router';
+import { Show, Suspense } from 'solid-js';
 
 
 const DashboardOutlet: Component = () => {
+  const isRouting = useIsRouting();
+
   return (
     <DashboardPage>
-      <Outlet />
+      <Show when={!isRouting()}>
+        <Suspense fallback={<FetchFallback spinner={DotSpinner}>Loading component</FetchFallback>}>
+          <Outlet />
+        </Suspense>
+      </Show>
     </DashboardPage>
   );
 };
