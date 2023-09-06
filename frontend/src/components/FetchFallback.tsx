afbc54607f7d13299e1ef3bdc6538b8ffff47b71
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2023-09-06 18:51:44 +0200

    Added option to pass custom spinner to FetchFallback

--- frontend/src/components/FetchFallback.tsx
+++ frontend/src/components/FetchFallback.tsx
@@ -1,13 +1,23 @@
-import { JSX } from 'solid-js';
+import { JSX, mergeProps } from 'solid-js';
 import CircleSpinner from '#components/CircleSpinner';
 
 import style from '#styles/FetchFallback.module.scss';
 
 
-const FetchFallback: ParentComponent<JSX.HTMLAttributes<HTMLDivElement>> = (props) => {
+export type FetchFallbackProps = JSX.HTMLAttributes<HTMLDivElement> & {
+  spinner?: Component;
+};
+
+const defaultProps: Required<Pick<FetchFallbackProps, 'spinner'>> = {
+  spinner: CircleSpinner,
+};
+
+const FetchFallback: ParentComponent<FetchFallbackProps> = (userProps) => {
+  const props = mergeProps(defaultProps, userProps);
+
   return (
-    <div class={[style.loading, props.class].join(' ')} {...props} >
-      <CircleSpinner />
+    <div class={[style.loading, props.class].join(' ')} {...props}>
+      <props.spinner />
       <span>{props.children}</span>
     </div>
   );
