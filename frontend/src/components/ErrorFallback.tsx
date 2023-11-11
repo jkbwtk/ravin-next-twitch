77db5f978fa5d47522e8b0746d3dd781e79f41cd
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2023-11-11 23:13:41 +0100

    Add horizontal option to ErrorFallback component

--- frontend/src/components/ErrorFallback.tsx
+++ frontend/src/components/ErrorFallback.tsx
@@ -1,19 +1,29 @@
-import { JSX } from 'solid-js';
+import { JSX, mergeProps } from 'solid-js';
 import Button from '#components/Button';
 
 import style from '#styles/ErrorFallback.module.scss';
+import { RequiredDefaults } from '#shared/utils';
 
 
-export type ErrorFallbackProps = JSX.HTMLAttributes<HTMLDivElement> & {
+export type ErrorFallbackProps = {
   refresh: () => void;
   loading: boolean;
+  horizontal?: boolean;
 };
 
-const ErrorFallback: ParentComponent<ErrorFallbackProps> = (props) => {
+const defaultProps: RequiredDefaults<ErrorFallbackProps> = {
+  horizontal: false,
+};
+
+const ErrorFallback: ParentComponent<ErrorFallbackProps & JSX.HTMLAttributes<HTMLDivElement>> = (userProps) => {
+  const props = mergeProps(defaultProps, userProps);
+
   return (
-    <div {...props} class={[style.error, props.class].join(' ')}>
+    <div {...props} class={[style.error, props.class].join(' ')} classList={{
+      [style.horizontal]: props.horizontal,
+    }}>
       <span>{props.children}</span>
-      <Button onClick={props.refresh} symbol='Refresh' loading={props.loading} color='primary'>Retry</Button>
+      <Button class={style.button} onClick={props.refresh} symbol='Refresh' loading={props.loading} color='primary'>Retry</Button>
     </div>
   );
 };
