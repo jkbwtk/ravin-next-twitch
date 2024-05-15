68f33af0b62b99bbbf250eff5755ea5f6780af72
Jakub Wiątek <jakub.wiatek@student.pk.edu.pl>
2024-05-15 18:11:33 +0200

    Linting workaround

--- frontend/src/locales/en-us/botActions.tsx
+++ frontend/src/locales/en-us/botActions.tsx
@@ -29,89 +29,92 @@ export const botActions = new Dictionary<BotActionType, string>('Unknown Action'
   [BotActionType.CommandTimerFailedLines]: 'Command Timer Failed Lines',
 });
 
-export const botActionsDescriptions = new Dictionary<BotActionType, (data: string[]) => string | JSX.Element>(
+
+type BotActionsDescriptionsReturnType = (data: string[]) => string | JSX.Element;
+
+export const botActionsDescriptions = new Dictionary<BotActionType, BotActionsDescriptionsReturnType>(
   (data) => (<span>Unknown action performed. Raw data: <code>{data.join(' ')}</code></span>),
-{
-  [BotActionType.Unknown]: (data) => (<span>Unknown action performed. Raw data: <code>{data.join(' ')}</code></span>),
+  {
+    [BotActionType.Unknown]: (data) => (<span>Unknown action performed. Raw data: <code>{data.join(' ')}</code></span>),
 
-  [BotActionType.ChannelJoined]: (data) => (<span>Channel <code>{data.at(0)}</code> joined</span>),
-  [BotActionType.ChannelLeft]: (data) => (<span>Channel <code>{data.at(0)}</code> left</span>),
+    [BotActionType.ChannelJoined]: (data) => (<span>Channel <code>{data.at(0)}</code> joined</span>),
+    [BotActionType.ChannelLeft]: (data) => (<span>Channel <code>{data.at(0)}</code> left</span>),
 
-  [BotActionType.CustomCommandExecuted]: (data) => (<span>Custom command <code>{data.at(0)}</code> called by <code>{data.at(1)}</code></span>),
+    [BotActionType.CustomCommandExecuted]: (data) => (<span>Custom command <code>{data.at(0)}</code> called by <code>{data.at(1)}</code></span>),
 
-  [BotActionType.CustomCommandFailedError]: (data) => (
-    <span>
+    [BotActionType.CustomCommandFailedError]: (data) => (
+      <span>
       Failed to execute custom command <code>{data.at(0)}</code> called by <code>{data.at(1)}</code> due to <code>{data.at(2)}</code>
-    </span>
-  ),
-  [BotActionType.CustomCommandFailedCooldown]: (data) => (
-    <span>
+      </span>
+    ),
+    [BotActionType.CustomCommandFailedCooldown]: (data) => (
+      <span>
       Failed to execute custom command <code>{data.at(0)}</code> called by <code>{data.at(1)}</code> due to cooldown
-    </span>
-  ),
-  [BotActionType.CustomCommandFailedUserLevel]: (data) => (
-    <span>
+      </span>
+    ),
+    [BotActionType.CustomCommandFailedUserLevel]: (data) => (
+      <span>
       Failed to execute custom command <code>
-        {data.at(0)}
-      </code> called by <code>
-        {data.at(1)}
-      </code> due to insufficient user level. User's level: <code>
-        {userLevels.getCoerced(data.at(2))}
-      </code>
-    </span>
-  ),
-  [BotActionType.CustomCommandFailedDisabled]: (data) => (
-    <span>
+          {data.at(0)}
+        </code> called by <code>
+          {data.at(1)}
+        </code> due to insufficient user level. User's level: <code>
+          {userLevels.getCoerced(data.at(2))}
+        </code>
+      </span>
+    ),
+    [BotActionType.CustomCommandFailedDisabled]: (data) => (
+      <span>
       Failed to execute custom command <code>{data.at(0)}</code> called by <code>{data.at(1)}</code> because it is disabled
-    </span>
-  ),
+      </span>
+    ),
 
-  [BotActionType.ChantingDetected]: (data) => (
-    <span>
+    [BotActionType.ChantingDetected]: (data) => (
+      <span>
       Chanting detected: <code>{data.at(0)}</code> (length: {data.at(1)})
-    </span>
-  ),
+      </span>
+    ),
 
-  [BotActionType.FilteredPhrase]: (data) => (
-    <span>
+    [BotActionType.FilteredPhrase]: (data) => (
+      <span>
       Filtered phrase <code>
-        {data.at(1)}
-      </code> with <code>
-        {Number(data.at(2)).toFixed(0)}%
-      </code> similarity to <code>
-        {data.at(0)}
-      </code> by <code>{data.at(3)}</code>. Action taken: <code>{actions.getCoerced(data.at(4))}</code>
-    </span>
-  ),
+          {data.at(1)}
+        </code> with <code>
+          {Number(data.at(2)).toFixed(0)}%
+        </code> similarity to <code>
+          {data.at(0)}
+        </code> by <code>{data.at(3)}</code>. Action taken: <code>{actions.getCoerced(data.at(4))}</code>
+      </span>
+    ),
 
-  [BotActionType.FilteredRegex]: (data) => (
-    <span>
+    [BotActionType.FilteredRegex]: (data) => (
+      <span>
       Regex <code>
-        {data.at(0)}
-      </code> matched phrase <code>
-        {data.at(1)}
-      </code> by <code>{data.at(2)}</code>. Action taken: <code>{actions.getCoerced(data.at(3))}</code>
-    </span>
-  ),
+          {data.at(0)}
+        </code> matched phrase <code>
+          {data.at(1)}
+        </code> by <code>{data.at(2)}</code>. Action taken: <code>{actions.getCoerced(data.at(3))}</code>
+      </span>
+    ),
 
-  [BotActionType.CommandTimerExecuted]: (data) => (
-    <span>
+    [BotActionType.CommandTimerExecuted]: (data) => (
+      <span>
       Command timer <code>{data.at(0)}</code> executed
-    </span>
-  ),
-  [BotActionType.CommandTimerExecutedCommand]: (data) => (
-    <span>
+      </span>
+    ),
+    [BotActionType.CommandTimerExecutedCommand]: (data) => (
+      <span>
       Command timer <code>{data.at(0)}</code> called by <code>{data.at(2)}</code>
-    </span>
-  ),
-  [BotActionType.CommandTimerFailedError]: (data) => (
-    <span>
+      </span>
+    ),
+    [BotActionType.CommandTimerFailedError]: (data) => (
+      <span>
       Command timer <code>{data.at(0)}</code> failed due to <code>{data.at(1)}</code>
-    </span>
-  ),
-  [BotActionType.CommandTimerFailedLines]: (data) => (
-    <span>
+      </span>
+    ),
+    [BotActionType.CommandTimerFailedLines]: (data) => (
+      <span>
       Command timer <code>{data.at(0)}</code> failed due to insufficient lines between messages. Current lines count: <code>{data.at(1)}</code>
-    </span>
-  ),
-});
+      </span>
+    ),
+  });
