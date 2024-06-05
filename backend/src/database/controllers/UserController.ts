b2fb9515456433bd93edfee8965776281be8b159
Jakub Wiątek <jakub.wiatek@student.pk.edu.pl>
2024-06-06 00:50:28 +0200

    Port user table related logic

---
+++ backend/src/database/controllers/UserController.ts
@@ -0,0 +1,48 @@
+import { eq, getTableColumns } from 'drizzle-orm';
+import { db } from '#database/database';
+import { channelsTable, usersTable } from '#shared/schema/schema';
+import { trackQueryPerformance } from '#database/utils';
+
+
+export type Channel = typeof channelsTable.$inferSelect;
+export type User = typeof usersTable.$inferSelect;
+
+export type UserController = typeof UserControllerTarget;
+
+const UserControllerTarget = {
+  async createChannel(userId: string): Promise<Channel | null> {
+    const query = db
+      .insert(channelsTable)
+      .values({ userId })
+      .returning(getTableColumns(channelsTable))
+      .onConflictDoNothing();
+
+    const result = await query;
+
+    return result.at(0) ?? null;
+  },
+
+  async getById(id: string): Promise<User | null> {
+    const query = db
+      .query.usersTable.findFirst({
+        where: eq(usersTable.id, id),
+      });
+
+    const result = await query;
+
+    return result ?? null;
+  },
+
+  async getByLogin(login: string): Promise<User | null> {
+    const query = db
+      .query.usersTable.findFirst({
+        where: eq(usersTable.login, login),
+      });
+
+    const result = await query;
+
+    return result ?? null;
+  },
+} as const;
+
+export const UserController = trackQueryPerformance('UserController', UserControllerTarget);
