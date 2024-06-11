807fd021ce0adb2033301a266c3a5a883f600397
Jakub Wiątek <jakub.wiatek@student.pk.edu.pl>
2024-06-11 23:36:52 +0200

    Fix alias import priority

--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,6 @@ export default defineConfig({
 
   resolve: {
     alias: [
-      { find: '#shared', replacement: resolve(cwd(), 'shared/src') },
       { find: '#types', replacement: resolve(cwd(), 'shared/src/types') },
       { find: '#schema', replacement: '#shared/src/schema' },
       { find: '#components', replacement: resolve(frontendDevelopmentPath, 'src/components') },
@@ -47,6 +46,7 @@ export default defineConfig({
       { find: '#providers', replacement: resolve(frontendDevelopmentPath, 'src/providers') },
       { find: '#routers', replacement: resolve(frontendDevelopmentPath, 'src/routers') },
       { find: '#locales', replacement: resolve(frontendDevelopmentPath, 'src/locales') },
+      { find: '#shared', replacement: resolve(cwd(), 'shared/src') },
     ],
   },
 
